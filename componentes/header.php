823b8077e741db7ab528191be7ec20c9ac6f572b
Luisa Migueres <lumigueres@gmail.com>
2018-09-28 18:19:37 -0300

    Adicionando header

---
+++ componentes/header.php
@@ -0,0 +1,20 @@
+
+<link rel="stylesheet" href="./css/header.css">
+
+<header class="row main-header main-header-align linha">
+
+    <img class="logo col-md-1 col-md-offset-1 col-xs-12" src="./images/logo_maxperience.png" alt="logotipo" class="logo img-circle">
+
+    <div class="input-group col-md-4">
+      <input type="text" class="form-control" placeholder="O que você está procurando?">
+      <span class="input-group-btn search-button">
+        <button class="btn btn-default" type="button">Buscar</button>
+      </span>
+    </div>
+
+    <div class="btn-group col-md-3" role="group" aria-label="...">
+      <button type="button" class="btn btn-default header-button">Entrar</button>
+      <button type="button" class="btn btn-default header-button">Cadastrar</button>
+    </div>
+
+</header>
