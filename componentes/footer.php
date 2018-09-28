698c93db5e1591d3727317946124d1632fa6a27b
Luisa Migueres <lumigueres@gmail.com>
2018-09-28 18:19:56 -0300

    Adicionando footer

---
+++ componentes/footer.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="./css/footer.css">
+<footer class="footer-main">
+  <ul class="nav nav-pills">
+    <li role="presentation"><a href="#">QUEM SOMOS</a></li>
+    <li role="presentation"><a href="faq.html">FAQ</a></li>
+  </ul>
+</footer>
